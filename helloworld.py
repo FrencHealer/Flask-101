f55db736836fb571a88097eb190b69b2089533dc
FrencHealer <mackowiczhugo@gmail.com>
2023-11-22 09:51:15 +0100

    Insertion API request

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import datetime
+import http.client
+import json
 
 app = Flask(__name__, template_folder='templates', static_folder='staticfiles')
 
@@ -8,7 +11,12 @@ def ciel():
 
 @app.route('/snir')
 def snir():
-    return render_template('snir.html')
+    connexion = http.client.HTTPSConnection('api.openweathermap.org')
+    connexion.request("GET", "/data/2.5/weather?lat=" + str(48.3)+"&lon="+ str(4.08)+ "&APPID=af8ac9e76a4dee9b5ef91bcd83a77ee9&mode=json&units=metric&lang=fr")
+    rep = connexion.getresponse()
+    rep_str=rep.read()
+    jsonDico = json.loads(rep_str.decode('utf-8'))
+    return render_template('snir.html',utc_dt=str(jsonDico["main"]["temp"]))
 
 @app.route('/etudes-sup')
 def etudes_sup():
